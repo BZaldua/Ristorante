5d1e152f6bdabdd945de782f8059b09acb545ecf
BZaldua <benatzaldua1775@gmail.com>
2018-01-04 11:06:24 +0100

    Boton generar factura + insercion en BD

--- app/src/main/java/iti/ehu/ristorante/CommandActivity.java
+++ app/src/main/java/iti/ehu/ristorante/CommandActivity.java
@@ -2,9 +2,12 @@ package iti.ehu.ristorante;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -18,8 +21,12 @@ public class CommandActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_command);
 
+        // Cargar los datos del anterior intent
         HashMap<String, Integer> comanda = (HashMap<String, Integer>) getIntent().getSerializableExtra("comanda");
 
+
+        // Instancia base de datos
+        final ModelManagement db = new ModelManagement(this);
 
         // Crear el ListView para la interfaz
         ListView mListViewCommand = (ListView)findViewById(R.id.list_view_command);
@@ -30,6 +37,9 @@ public class CommandActivity extends AppCompatActivity {
         // Separar los datos de comanda
         ArrayList<String> command_aux = new ArrayList<String>();
         ArrayList<Float> unidad_aux = new ArrayList<Float>();
+
+        ArrayList<Dish> platos_lista = new ArrayList<Dish>();
+
         float total = 0;
         float precio_p = 0;
         Iterator it = comanda.entrySet().iterator();
@@ -40,6 +50,14 @@ public class CommandActivity extends AppCompatActivity {
             String[] s = (e.getKey().toString()).split("\\(");
             String aux = s[1].substring(0,s[1].length()-1);
             total  = total + (Float.parseFloat(e.getValue().toString()) * Float.parseFloat(aux));
+
+            // Para la comanda
+            Dish aux_dish = new Dish();
+            aux_dish.setName(e.getKey().toString());
+            aux_dish.setPrice(Float.parseFloat(e.getValue().toString()) * Float.parseFloat(aux));
+            platos_lista.add(aux_dish);
+
+
             System.out.println(e.getKey() + " " + e.getValue());
         }
 
@@ -61,6 +79,21 @@ public class CommandActivity extends AppCompatActivity {
 
         TextView text = (TextView) findViewById(R.id.text_total);
         text.setText("Total: " + total + " €");
+
+        final Command new_command = new Command(platos_lista);
+
+
+        Button btn_ticket = (Button) findViewById(R.id.btn_ticket);
+        btn_ticket.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Guardar datos en BD
+                db.addCommand(new_command);
+                System.out.println("Guardado");
+
+                Toast.makeText(CommandActivity.this, "Se ha generado la factura y se ha enviado el menu a cocina", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
 }
